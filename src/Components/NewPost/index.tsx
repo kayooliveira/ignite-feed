c8a052a1875d4e5b502150ca8bcdcc7df9978d48
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-16 18:10:53 -0300

    fix(Fixing responsible on  newPost component): :bug:

--- src/Components/NewPost/index.tsx
+++ src/Components/NewPost/index.tsx
@@ -11,15 +11,15 @@ export function NewPost() {
           placeholder={`O que está pensando, ${authUser.name.split(' ')[0]}?`}
           className="w-full resize-none rounded-lg border-[1px] border-primary/50 bg-background p-4 font-bold text-gray-500  outline-none ring-2 ring-transparent ring-offset-2 ring-offset-background focus:border-transparent focus:ring-secondary"
         ></textarea>
-        <div className="flex w-full basis-72 items-center justify-evenly gap-4 py-4 text-gray-500">
-          <button className="flex items-center text-xl font-bold transition-colors hover:text-gray-700 ">
+        <div className="flex w-full flex-row items-center justify-evenly gap-4 py-4 text-gray-500">
+          <button className="flex items-center text-xl font-bold transition-colors hover:text-gray-700">
             <AiFillCamera className="mr-3 h-10 w-10" />
             Foto/Video
           </button>
           <button className="flex items-center text-xl font-bold transition-colors hover:text-gray-700 ">
             <AiFillVideoCamera className="mr-3 h-10 w-10" /> Live
           </button>
-          <button className="flex items-center text-xl font-bold transition-colors hover:text-gray-700 ">
+          <button className="hidden items-center text-xl font-bold transition-colors hover:text-gray-700 md:flex ">
             <AiFillHeart className="mr-3 h-10 w-10" /> Sentimento
           </button>
         </div>
