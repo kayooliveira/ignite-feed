698679f9ed591c864b4e8c498b210405ef71561b
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-16 17:46:50 -0300

    feat(Creating new post component): :sparkles:

---
+++ src/Components/NewPost/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { AiFillCamera, AiFillHeart, AiFillVideoCamera } from 'react-icons/ai'
+
+import { authUser } from '../../utils/mock'
+
+export function NewPost() {
+  return (
+    <>
+      <div className="rounded-lg border-[1px] border-gray-500/20 p-8">
+        <textarea
+          placeholder={`O que está pensando, ${authUser.name.split(' ')[0]}?`}
+          className="w-full resize-none rounded-lg border-[1px] border-primary/50 bg-background p-4 font-bold text-gray-500  outline-none ring-2 ring-transparent ring-offset-2 ring-offset-background focus:border-transparent focus:ring-secondary"
+        ></textarea>
+        <div className="flex w-full basis-72 items-center justify-evenly gap-4 py-4 text-gray-500">
+          <button className="flex items-center text-xl font-bold transition-colors hover:text-gray-700 ">
+            <AiFillCamera className="mr-3 h-10 w-10" />
+            Foto/Video
+          </button>
+          <button className="flex items-center text-xl font-bold transition-colors hover:text-gray-700 ">
+            <AiFillVideoCamera className="mr-3 h-10 w-10" /> Live
+          </button>
+          <button className="flex items-center text-xl font-bold transition-colors hover:text-gray-700 ">
+            <AiFillHeart className="mr-3 h-10 w-10" /> Sentimento
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/Components/RightContent/index.tsx
+++ src/Components/RightContent/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import { posts } from '../../utils/mock'
+import { NewPost } from '../NewPost'
 import { Post } from '../Post'
 
 export function RightContent() {
   return (
     <main className="flex w-full flex-col gap-8">
+      <NewPost />
       {posts.map(post => (
         <Post
           key={post.author.id}
