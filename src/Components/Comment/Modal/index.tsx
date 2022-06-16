1c254afd0151a14dca9ab12963d2845269d49177
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-16 17:01:02 -0300

    feat(Creating modal to confirm comment delete): :sparkles:

---
+++ src/Components/Comment/Modal/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+interface IModal {
+  trigger: JSX.Element
+}
+
+export function Modal({ trigger }: IModal) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleModal = () => {
+    if (!isOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'unset'
+    }
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <>
+      <div onClick={toggleModal}>{trigger}</div>
+      {isOpen && (
+        <div className="scroll fixed left-0 top-0 z-40 flex min-h-screen w-screen items-center justify-center bg-black/90">
+          <div
+            id="modal"
+            className="relative w-[432px] rounded-lg bg-backgroundLight p-8"
+          >
+            <h2 className="mb-4 text-center text-3xl font-bold text-white">
+              Excluir comentário
+            </h2>
+            <p className="text-center text-lg text-white">
+              Você tem certeza que gostaria de excluir esse comentário?
+            </p>
+            <div className="mt-8 flex justify-center gap-2">
+              <button
+                onClick={toggleModal}
+                className="rounded-lg py-4 px-8 text-center font-bold text-white transition-colors hover:bg-backgroundComment focus:bg-backgroundComment"
+              >
+                Cancelar
+              </button>
+              <button
+                onClick={toggleModal}
+                autoFocus
+                className="rounded-lg py-4 px-8 text-center font-bold text-red-500 transition-colors hover:bg-background focus:bg-backgroundComment"
+              >
+                Sim, excluir
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
