02e523e31447a91f1902f4d3f07fb10680cc5d55
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-18 22:28:56 -0300

    feat(Creating hook to access auth context): :sparkles:

---
+++ src/Hooks/useAuth.ts
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+
+import { AuthContext } from '../@types/Contexts/AuthContext'
+
+export function useAuth() {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error("This component haven't access to this context")
+  }
+
+  return context
+}
