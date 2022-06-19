4bcee32f76965858501dea6315cc2ef393132a4c
Kayo Oliveira <g.kayooliveira123@gmail.com>
2022-06-19 15:38:42 -0300

    refactor(Refactoring post component): :recycle:

--- src/Components/Post/index.tsx
+++ src/Components/Post/index.tsx
@@ -1,8 +1,8 @@
 import { format, formatDistanceToNow, formatISO } from 'date-fns'
 import ptBR from 'date-fns/esm/locale/pt-BR/index.js'
 import Parser from 'html-react-parser'
-import React, { ChangeEvent, FormEvent, useState } from 'react'
-import { AiFillLike, AiOutlineLike } from 'react-icons/ai'
+import React, { ChangeEvent, FormEvent, useRef, useState } from 'react'
+import { AiFillLike, AiFillWechat, AiOutlineLike } from 'react-icons/ai'
 
 import { CommentType, PostType } from '../../@types'
 import { useAuth } from '../../Hooks/useAuth'
@@ -19,11 +19,15 @@ export function Post({ post, handleLikePost, handleUnlikePost }: PostProps) {
   const [comments, setComments] = useState<CommentType[]>(post.comments)
   const [commentInput, setCommentInput] = useState<string>('')
   const [isLikedPost, setIsLikedPost] = useState(false)
+  const newCommentRef = useRef<HTMLTextAreaElement>(null)
   const { state } = useAuth()
   const postDateFormatted = format(post.createdAt, "d 'de' LLL 'às' HH:m'h'", {
     locale: ptBR
   })
-  const postDateFromNow = formatDistanceToNow(post.createdAt, { locale: ptBR })
+  const postDateFromNow = formatDistanceToNow(post.createdAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
 
   function handleChange(e: ChangeEvent<HTMLTextAreaElement>) {
     setCommentInput(e.target.value)
@@ -59,89 +63,112 @@ export function Post({ post, handleLikePost, handleUnlikePost }: PostProps) {
     setIsLikedPost(true)
   }
 
+  function focusCommentInput() {
+    newCommentRef.current?.focus()
+  }
+
   return (
-    <article className="rounded-lg bg-backgroundLight p-8">
-      <header id="post-header" className="flex flex-wrap justify-between">
-        <div className="flex">
-          <div className="w-fit cursor-pointer rounded-lg border-2 border-primary bg-backgroundLight p-1 transition-all hover:-translate-y-1">
-            <img
-              src={post.author.profilePhoto}
-              alt={post.author.name + ' profile photo'}
-              className="w-24 md:w-16"
-            />
-          </div>
-          <div id="author-info" className="ml-3 flex flex-col">
-            <p className="cursor-pointer text-lg font-bold text-white">
+    <article className="flex-1 rounded-lg bg-backgroundLight p-8">
+      <header className="flex w-full flex-row items-start justify-between md:flex-row">
+        <div id="post-author" className="flex w-fit flex-1 items-start">
+          <img
+            src={post.author.profilePhoto}
+            alt={post.author.name + ' avatar'}
+            className="h-10 w-10 md:h-16 md:w-16"
+          />
+          <div id="post-author-info" className="ml-3 flex flex-col">
+            <p className="align-top font-bold leading-4 text-white md:text-xl">
               {post.author.name}
             </p>
-            <span className="cursor-pointer  text-sm text-gray-600">
+            <span className="text-xs leading-5 text-gray-600 md:text-sm">
               {post.author.job} | <strong>{post.author.company}</strong>
             </span>
           </div>
         </div>
-
-        <div id="post-time" className="flex flex-col items-end">
+        <div id="post-time-info" className="flex flex-col items-end">
           <time
-            className="text-gray-600"
-            title={postDateFormatted}
+            className="text-xs leading-3 text-gray-600 md:text-base"
             dateTime={formatISO(post.createdAt)}
+            title={'publicado em ' + postDateFormatted}
           >
-            publicado há {postDateFromNow}
+            {postDateFromNow}
           </time>
-          <div id="post-actions">
-            <button
-              title={
-                isLikedPost ? 'Desmarcar como gostei' : 'Marcar como gostei'
-              }
-              className="color-gray-600 py-2 font-bold text-gray-600"
-              onClick={likePost}
-            >
-              <div className="gap-2">
-                {isLikedPost ? (
-                  <p className="text-primary">
-                    <AiFillLike className="inline-block h-6 w-6 align-bottom" />{' '}
-                    • {post.likes}
-                  </p>
-                ) : (
-                  <p>
-                    <AiOutlineLike className="inline-block h-6 w-6 align-bottom" />{' '}
-                    • {post.likes}
-                  </p>
-                )}
-              </div>
-            </button>
-          </div>
+          <button
+            onClick={likePost}
+            className={`nter flex items-center leading-8 ${
+              isLikedPost ? 'text-primary' : 'text-gray-600'
+            }`}
+          >
+            {isLikedPost ? (
+              <>
+                <AiFillLike className="mr-1 h-5 w-5" />
+              </>
+            ) : (
+              <>
+                <AiOutlineLike className="mr-1 h-5 w-5" />
+              </>
+            )}
+            <span>• {post.likes}</span>
+          </button>
         </div>
       </header>
-      <h4 id="post-content" className="my-4 text-white">
+      <section id="post-content" className="py-4 text-white">
         {Parser(post.content)}
-      </h4>
-      <hr className="border-gray-600" />
-      <form onSubmit={handleAddNewComment} id="post-reply">
-        <p className="my-4 text-lg font-bold text-white">Deixe seu feedback</p>
-        <textarea
-          onChange={handleChange}
-          value={commentInput}
-          className="w-full resize-none rounded-lg border-[1px] border-primary bg-background p-4 text-white outline-none ring-2 ring-transparent ring-offset-2 ring-offset-backgroundLight focus:border-transparent focus:ring-secondary"
-        ></textarea>
-        {commentInput.trim() !== '' && (
-          <button
-            type="submit"
-            title="Publicar novo comentário"
-            className="my-4 rounded-lg bg-primary p-3 text-lg font-bold text-white transition-all hover:bg-secondary"
-          >
-            Publicar
-          </button>
-        )}
-      </form>
-
-      {comments.map(comment => (
-        <Comment
-          key={comment.id}
-          comment={comment}
-          handleDeleteComment={handleDeleteComment}
-        />
-      ))}
+      </section>
+      <hr className="my-4 border-gray-600" />
+      <div id="post-comments">
+        <p className="my-4 text-xl font-bold text-white">Deixe seu feedback</p>
+        <form onSubmit={handleAddNewComment}>
+          <textarea
+            ref={newCommentRef}
+            value={commentInput}
+            onChange={handleChange}
+            className="w-full resize-none rounded-lg border-[1px] border-primary bg-background p-4 font-bold text-gray-600 outline-none ring-2 ring-transparent ring-offset-2 ring-offset-transparent focus:border-transparent focus:ring-secondary"
+          ></textarea>
+          {commentInput.trim() !== '' && (
+            <button className="mt-4 rounded-lg bg-primary py-3 px-6 font-bold text-white">
+              Publicar
+            </button>
+          )}
+        </form>
+        {comments.map(comment => (
+          <Comment
+            key={comment.id}
+            comment={comment}
+            handleDeleteComment={handleDeleteComment}
+          />
+        ))}
+      </div>
+      <hr className="my-4 border-gray-600" />
+      <div
+        id="post-actions"
+        className="flex w-full items-center justify-evenly border-gray-600 text-center"
+      >
+        <button
+          onClick={likePost}
+          className={`flex items-center transition-colors hover:text-primary ${
+            isLikedPost ? 'text-primary' : 'text-white'
+          }`}
+        >
+          {isLikedPost ? (
+            <>
+              <AiFillLike className="mr-1 h-6 w-6" />
+            </>
+          ) : (
+            <>
+              <AiOutlineLike className="mr-1 h-6 w-6" />
+            </>
+          )}
+          <span className="font-bold">Curtir • {post.likes}</span>
+        </button>
+        <button
+          onClick={focusCommentInput}
+          className={`flex items-center text-white transition-colors hover:text-primary `}
+        >
+          <AiFillWechat className="mr-1 h-6 w-6" />
+          <span className="font-bold">Comentar • {comments.length} </span>
+        </button>
+      </div>
     </article>
   )
 }
